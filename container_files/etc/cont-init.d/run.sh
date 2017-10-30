6fb684619942d39882786dff14087c75d98e6f04
Docker Sidekick Container <bcone+docker_sidekick@esu10.org>
2017-10-30 10:21:27 -0500

    Pretty significant rewrite of the run.sh file to make it more generic and usable across multiple platforms and usefulnesses

--- container_files/etc/cont-init.d/run.sh
+++ container_files/etc/cont-init.d/run.sh
@@ -1,14 +1,34 @@
 #!/usr/bin/with-contenv /bin/sh
 
-echo "Directory received is ${1}"
-# DIR="/tmp"
-# $1 is the working directory
-# $2 directory where we'll clone
-# $3 is the repo url
+##################################################################
+# This script does some moderately useful things, most notably   #
+# is that it is configured to work in a sidekick container to    #
+# start, pull down from a git repo, quit.                        #
+#                                                                #
+# All configuration directives are expected to be available as   #
+# variables. You can pass them in as arguments to the script if  #
+# you are calling this script explicitly but preference is given #
+# to environment variables.                                      #
+##################################################################
+
+#############################################################################################
+# Expected Variables:                                                                       #
+#   If you are passing variables as command arguments, they should be in the order below    #
+# 1) WORKING_DIR - REQUIRED - the directory in which you want the script to execute.        #
+# 2) GIT_REPO_DIR - OPTIONAL - Directory where you want to install the repo, if you want a  #
+#   folder other than the repo name.                                                        #
+# 3) GIT_REPO_URL - REQUIRED - The url of the repo which you will be cloning.               #
+# 4) GIT_REPO_BRANCH - OPTIONAL, defaults to Master Branch you want to checkout.            #
+# 5) DEPLOY_PUB_KEY - REQUIRED, allows placement of pub key.                                #
+# 6) DEPLOY_PRI_KEY_LOCATION - OPTIONAL - defaults to /run/secrets from Rancher config.     #
+# 7) KNOWN_HOSTS - REQUIRED - host identity of the git repo server.
+#############################################################################################
 
 # Takes a path, relative or absolute and ensures that it exists
 # Wich means that it creates any directories needed so that the full
 # supplied structure exists upon exit.
+
+# As it turns out, you can do all of this using mkdir -p. Leaving this script here because I like it.
 ensure_directory() {
     DIRECTORY_PATH=$1
     echo "${DIRECTORY_PATH}"
@@ -46,91 +66,127 @@ ensure_directory() {
 
     fi
 }
-if [ $# -eq 0 ]; then
-    echo "No directory argument supplied. Using current directory."
-elif [ ! -d "$1" ]; then
-    echo "The supplied directory ${1} does not exist"
+
+##########################################################################
+# Let's handle the fail conditions first and save doing them later.      #
+# This way, if we get past this section of the script then we know that  #
+# everything is in place as expected. Fail-able conditions are:          #
+# * WORKING_DIR not supplied                                             #
+# * DEPLOY_PUB_KEY not supplied                                          #
+# * GIT_REPO_URL not supplied                                            #
+# * Private Key not exists                                               #
+##########################################################################
+DEPLOY_PUB_KEY=${DEPLOY_PUB_KEY:=$5}
+
+GIT_REPO_URL=${GIT_REPO_URL:=$3}
+
+DEPLOY_PRI_KEY_LOCATION=${DEPLOY_PRI_KEY_LOCATION:=$6}
+
+KNOWN_HOSTS=${KNOWN_HOSTS:=$7}
+
+# Some debugging things - write all variables out.
+
+# echo "WORKING_DIR: ${WORKING_DIR}"
+# echo "GIT_REPO_DIR: ${GIT_REPO_DIR}"
+# echo "GIT_REPO_URL: ${GIT_REPO_URL}"
+# echo "DEPLOY_PUB_KEY: ${DEPLOY_PUB_KEY}"
+# echo "DEPLOY_PRI_KEY_LOCATION: ${DEPLOY_PRI_KEY_LOCATION}"
+
+
+# if [[ ${DOTS} -lt 1 ]]
+# then
+#   echo 'The hostname you entered does not appear to be a valid FQDN, aborting!'
+#   exit 1
+# fi
+if [ -z "$DEPLOY_PUB_KEY" ]; then
+    echo "You did not supply a value for the public key. Any value will do at this point
+  of the script but you should know that an invalid key will make you fail later."
     exit 0
-else
-    echo "$1 exists. Changing working directory."
-    cd $1
-    pwd
+elif [ -z "$GIT_REPO_URL" ]; then
+    echo "You did not supply a git repo. I cannot clone a repo without an endpoint and I'm just
+  a lowly bash sript and don't have the ability to infer what you might have intended."
+    exit 0
+elif [ ! -e "${DEPLOY_PRI_KEY_LOCATION}/id_rsa" ]; then 
+    echo "We could not find a private key - assuming filename of \"id_rsa\" - at the supplied path. I'm sure you're aware,
+  but without a private key, asynchronous cryptography cannot take place. Therefore, this script must exit."
+    exit 0
+elif [ -z "$KNOWN_HOSTS" ]; then
+    echo "I've been taught not to talk to strangers which means that I clone repos from servers I don't know.
+  Please populate my known_hosts file by using the KNOWN_HOSTS variable or use argument 7 as a command arg."
+  exit 0
 fi
 
-#Run our install script to ensure that git is installed and configured.
-# bash /scripts/git_install_setup.sh
-
-#cd $1
-# Stage 1.
-        # Check for existence of key files.
-# if [ -e /Users/bcone/Development/logstash-docker/container_files/scripts/assets/id_rsa.pub ]; then
-if [ -e /scripts/assets/id_rsa.pub ]; then
-    if [ ! -d ~/.ssh ]; then
-        echo ".ssh directory does not exist. creating."
-        mkdir ~/.ssh
-        # Force in git.ops.esu10.org into the known hosts file.
-        cat >> ~/.ssh/known_hosts <<EOF 
-git.ops.esu10.org,204.234.24.180 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBGXqTFGIttY4u/LT49IWhWKHQu31DgfIVYxtMxPMGd2VPk/YcENc/gy3dZmFf6JRnbzUrLwbFDbp4EkRcDW26AU=
-EOF
+#1. Determine the working directory, first through environment, then through argument, fail to current dir.
+WORKING_DIR=${WORKING_DIR:=$1}
+echo "Working directory is ${WORKING_DIR}."
+if [ ! -z "$WORKING_DIR" ]; then
+    echo "We fell into the if case"
+    echo "Changing directories to ${WORKING_DIR}";
+    if [ ! -d ${WORKING_DIR} ]; then
+        echo ""
+        echo "The directory you specified does not exist so I am creating it. Please plan better next time."
+        echo ""
+        mkdir -p ${WORKING_DIR}
     fi
-    echo "copying id_rsa.pub to .ssh directory."
-    cp /scripts/assets/id_rsa.pub ~/.ssh/
+    cd $WORKING_DIR
+    echo "As proof that we changed directories, here is the current working direcory: $(pwd)."
 else
-    echo "Pub Key not found. Script cannot complete."
-    exit 0
-fi
-# Check for Private Key
-echo "Pub key file located. Checking for Private Key"
-if [ -e /run/secrets/id_rsa ]; then
-    echo "Private key file located. Yay, script can continue."
-    cp /run/secrets/id_rsa ~/.ssh/
-else
-    echo "Private key not found. Script cannot continue."
-    exit 0
+    echo "No directory received through variable or argument. Staying put."
 fi
 
-if [ -d ${LOGSTASH_DIR}/.git ]; then
+
+# echo "End of debugging area. Exiting."
+# exit 0
+# Run our install script to ensure that git is installed and configured.
+ bash /scripts/git_install_setup.sh
+
+# We've gotten this far, we know we have the required items we 
+# need and can start the actual usefulness of this script.
+if [ ! -d ~/.ssh ]; then
+    echo ".ssh directory does not exist. creating."
+    mkdir ~/.ssh
+    # Force in git.ops.esu10.org into the known hosts file.
+    # cat >> ~/.ssh/known_hosts <<EOF ${KNOWN_HOSTS}
+    echo "$KNOWN_HOSTS" > ~/.ssh/known_hosts
+fi
+# Let's make sure there isn't a key present first so that we don't overwrite it.
+if [ ! -e ~/.ssh/id_rsa.pub ]; then
+    echo "Writing Pub Key to .ssh directory."
+    echo "$DEPLOY_PUB_KEY" > ~/.ssh/id_rsa.pub
+    echo "Pub key file placed. Prepping to place Private Key"
+fi
+if [ ! -e ~/.ssh/id_rsa ]; then
+    echo "Private key file located. Yay, script can continue."
+    cp $DEPLOY_PRI_KEY_LOCATION/id_rsa ~/.ssh/
+fi
+echo
+echo "Inspecting ${WORKING_DIR}/${GIT_REPO_DIR} for a .git directory."
+echo
+if [ -d ${GIT_REPO_DIR}/.git ]; then
     # If the .git repo exists then this is not the first time that this script has run.
     # and our only job is to pull down any changes that might exist from the repo.
-    echo "Git repo exists in ${LOGSTASH_DIR} directory"
-    cd ${LOGSTASH_DIR}
-    git pull origin master
-    # git add . --all
-    # git commit -m "Nightly Commit for $(date)"
+    echo "Git repo exists in ${GIT_REPO_DIR} directory"
+    cd ${WORKING_DIR}/${GIT_REPO_DIR}
+    BRANCH=${GIT_REPO_BRANCH:="master"}
+    echo "git pull origin ${BRANCH}"
+    git pull origin ${BRANCH}
 else
     # There is no git directory so we assume this is the first time this script has run.
     # We need to do some initial setup.
-    # 1. Set up pub/private keys. Pub key is part of this repository; Pri key is assumed to be mounted 
-    #     through rancher as secret file named id_rsa and available at the rancher default.
-    # 2. Clone the repository down from the provided url.
-    # 3. Set up symlinks from the logstash expected location to our repo-provided directories
-    #     for config, pipeline, and patterns.
-    echo "No git directory in ${LOGSTASH_DIR}. Assuming this is the first time this script has run. Commencing initialization... "
-# Stage 2
-    if [ ! -z "$LOGSTASH_REPO_GIT_URL" ]; then
-        # Repo url no passed through a variable. Check argument 2.
-        echo "Repo url passed through a variable."
-        echo "${LOGSTASH_REPO_GIT_URL}"
-        REPO=$LOGSTASH_REPO_GIT_URL
-    elif [ ! -z "$2" ]; then
-        echo "Repo url passed through argument."
-        REPO=$2
-    else
-        echo "No git repository supplied, I don't want to assume where this might be so I have to conclude
-that I cannot continue in my efforts to set up this infrastructure."
-        exit 0
-    fi
-
-    echo "git clone ${REPO}"
-    git clone ${REPO} ${LOGSTASH_DIR}
+    echo "No git directory in ${WORKING_DIR}/${GIT_REPO_DIR}. Assuming this is the first time this script has run. Commencing initialization... "
+    echo 
+    echo "git clone ${GIT_REPO_URL} ${WORKING_DIR}/${GIT_REPO_DIR}"
+    git clone ${GIT_REPO_URL} ${WORKING_DIR}/${GIT_REPO_DIR}
     git config --global user.name "Docker Sidekick Container"
     git config --global user.email "bcone+docker_sidekick@esu10.org"
 
-# Stage 3
-    # Based on their documentation, Logstash expexts the configuration files (logstash directory)
-    # to exist at /usr/share/
 fi
 
+# Lastly, we need to figure out how to do some additional, LS specific commands w/o always doing them...
 #Do Rsync stuff to get config, pipeline, and patterns dirs in correct place
-ensure_directory /usr/share/logstash
-rsync -r ${LOGSTASH_DIR}/ /usr/share/logstash/
+if [ ! -z "$LOGSTASH_SPECIFIC_INSTANCE" ]; then
+    echo "This is a logstash specific instantiation that does logstash specific things."
+    ensure_directory /usr/share/logstash
+    echo "rsync ${WORKING_DIR}"
+    rsync -r ${WORKING_DIR}/${GIT_REPO_DIR}/ /usr/share/logstash/
+fi
